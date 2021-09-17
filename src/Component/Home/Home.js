3b9ceb2f8998dc5c74603451bd1cc095f0641ce8
iammahesh9919 <maheshpandey9919.github.com>
2021-09-17 16:58:03 +0530

    Create a React app and publish it to GitHub Pages

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Home.css'
 import myimage from '../../image/profiletrans.png';
+import homeimage from '../../image/home.jpg';
 
 export default class Home extends React.Component{
 
 	render(){
 		return (
 			<section className='home-nav'>
-				<img src="../../home.jpg" className='slider' />
+				<img src={homeimage} className='slider' />
 				
 				<img src={myimage} className='my-image' />				
 				<div className="slide-subtitle"><h4>I Am Mahesh Pandey</h4></div>
